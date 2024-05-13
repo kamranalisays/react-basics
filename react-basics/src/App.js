9199aa75be23394613ab41577d3801000600f533
KAMRAN\kamranali <kamranali.says@gmail.com>
2024-05-13 16:15:46 +0100

    Tutorial for Rendering List

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,4 +1,6 @@
+import PackingList from './Components/ConditionalRendering/PackingList';
 import Profile from './Components/Profile';
+import List from './Components/RenderingList/RenderingList';
 
 
 
@@ -6,7 +8,12 @@ import Profile from './Components/Profile';
 function App() {
   return (
    <div>
+    <h1>Tutorial : Passing properties to child component "Avatar"  </h1>
     <Profile/>
+    <h1>Tutorial : Conditional Rendering  </h1>
+    <PackingList/>
+    <h1> Tutorial : Rendering List Tutorial </h1>
+    <List/>
    </div>
   );
 }
